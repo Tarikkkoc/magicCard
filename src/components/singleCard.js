33c482a188839a6bbece77ccc1ca7c02c7974d50
Tarikkoc <ahmettarikkoc@gmail.com>
2023-01-27 12:46:50 +0300

    second-commit

--- src/components/singleCard.js
+++ src/components/singleCard.js
@@ -1,10 +1,13 @@
-import React from 'react'
+
 import './singleCard.css'
 
-export default function () {
+export default function SingleCard({ card }) {
   return (
+    <div className="card">
     <div>
-
+      <img src={card.src} className="front" alt="card front" /> {/* shuffleCardsın içerisinde en son oluşturulan card dizisinin srcsini ekledik.*/}
+      <img src="/img/cover.png" className="back" alt="card back" />
     </div>
+  </div>
   )
 }
