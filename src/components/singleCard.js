c9b2d22a9541e3526da305be600a38e5b8f7b793
Tarikkoc <ahmettarikkoc@gmail.com>
2023-01-30 19:44:20 +0300

    fourth

--- src/components/singleCard.js
+++ src/components/singleCard.js
@@ -1,14 +1,14 @@
 
 import './singleCard.css'
 
-export default function SingleCard({ card, handleChoice }) {
+export default function SingleCard({ card, handleChoice, flipped }) {
   const handleClick = () => {
-    handleChoice(card)
+    handleChoice(card) // kapalı karta tıklandığında açık resimli kartı atar. Arka yüze tıklandığında app.jsde handleChoice fonk. çalışır.
   }
 
   return (
     <div className="card">
-    <div>
+    <div className={flipped ? "flipped" : ""}>
       <img src={card.src} className="front" alt="card front" /> {/* shuffleCardsın içerisinde en son oluşturulan card dizisinin srcsini ekledik.*/}
       <img src="/img/cover.png"
        onClick={handleClick} 
